b0d367802ff017872537ebf0f91cc24ab28fbec2
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2021-12-29 11:03:59 -0300

    should NOT render create button when does not have role ADMIN

--- frontweb/src/pages/Admin/Employees/List/index.tsx
+++ frontweb/src/pages/Admin/Employees/List/index.tsx
@@ -8,6 +8,7 @@ import { SpringPage } from 'types/vendor/spring';
 import { Employee } from 'types/employee';
 import { AxiosRequestConfig } from 'axios';
 import { requestBackend } from 'util/requests';
+import { hasAnyRoles } from 'util/auth';
 
 const employeeHardCode = { // delete
   id: 1,
@@ -46,11 +47,16 @@ const List = () => {
 
   return (
     <>
-      <Link to="/admin/employees/create">
-        <button className="btn btn-primary text-white btn-crud-add">
-          ADICIONAR
-        </button>
-      </Link>
+
+      {
+        hasAnyRoles(['ROLE_ADMIN'])
+        && 
+        <Link to="/admin/employees/create">
+          <button className="btn btn-primary text-white btn-crud-add">
+            ADICIONAR
+          </button>
+        </Link>
+      }
 
       {page && page.content.map((employee, i) => (<EmployeeCard employee={employee} key={i} />))}
 
